64bca4763b566c9066cd54dceda5e372f6149d0d
Justin Byrne <theskaterjesus@gmail.com>
2021-01-23 16:55:40 +0000

    Added rotation when card is placed down

--- global.lua
+++ global.lua
@@ -37,6 +37,7 @@ end
 --[[ Checking when a card enters a zone --]]
 function onObjectEnterScriptingZone(zone, obj)
     if has_value(BUILD_PILE_GUIDS, zone.getGUID()) then
+        obj.setRotation(180)
         Wait.time(function() movePile(zone.getGUID()) end, 0.75)
     end
 end
